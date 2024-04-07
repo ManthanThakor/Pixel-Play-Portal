e22faa89227e3be236a48702c097969ba6b9cf9f
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-07 23:55:19 +0530

    title part

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const glowingText = (() => {
+    const textElement = document.getElementById('glowing_text');
+    let coloredText;
+    let index = 0;
+
+    const startGlowing = () => {
+        setInterval(() => {
+            const letters = textElement.textContent.split('');
+            if (letters[index] == ' ') {
+                index++;
+            }
+            letters[
+                index
+            ] = `<span style="color: var(--glowColor); text-shadow: var(--shadowColor);">${letters[index]}</span>`;
+            coloredText = letters.join('');
+            textElement.innerHTML = coloredText;
+            index = (index + 1) % letters.length;
+        }, 500);
+    };
+
+    return startGlowing;
+})();
+
+window.addEventListener('load', glowingText, false);
