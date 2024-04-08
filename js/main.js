70ffb98cf3e54bf6da5c9654003c298152da3ebf
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-08 13:40:11 +0530

    title part 3

--- js/main.js
+++ js/main.js
@@ -22,3 +22,45 @@ const glowingText = (() => {
 })();
 
 window.addEventListener('load', glowingText, false);
+
+
+// document.querySelector(".search-icon").addEventListener('click', (a) => {
+
+//  if (   document.querySelector(".search-box-one").innerHTML = "") {
+//     document.querySelector(".search-box-one").innerHTML = ('<input type="text" class="nav-link" placeholder="Search here..."/>');
+//  } 
+//  else {
+//     document.querySelector(".search-box-one").innerHTML = "";
+//  }
+// });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelector(".search-box").addEventListener('click', (a) => {
+//     const searchBox = document.querySelector(".search-box-one");
+  
+
+// console.log(a.currentTarget);
+
+//     if (searchBox.innerHTML === "") {
+//       searchBox.innerHTML = '<input type="text" class="nav-link" placeholder="Search here..."/>';
+//     } else {
+
+//       searchBox.innerHTML = "";
+     
+//     }
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".search-box").addEventListener('click', (event) => {
+      if (event.target.classList.contains('search-box') || (event.target.classList.contains('search-icon')) )   {
+        const searchBox = document.querySelector(".search-box-one");
+        if (searchBox.innerHTML === "") {
+          searchBox.innerHTML = '<input type="text" class="nav-link" placeholder="Search here..."/>';
+        } else {
+          searchBox.innerHTML = "";
+        }
+      }
+    });
+  });
