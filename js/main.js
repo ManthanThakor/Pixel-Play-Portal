92668522291d98ba98858975b4889ce741d48d6b
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-10 11:44:13 +0530

    Arcade part 1

--- js/main.js
+++ js/main.js
@@ -41,31 +41,29 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // ham burger
+   // ham burger
   
-  if (window.matchMedia("(max-width: 1165px)").matches) {
-    function toggleMenu() {
-      const hamPartSection = document.querySelector(".ham-part-section");
-      const pixelMenu = document.querySelector(".pixel-menu");
-      let isMenuVisible = false;
-  
-      hamPartSection.addEventListener("click", () => {
-        if (!isMenuVisible) {
-          pixelMenu.style.display = "flex"; // Show the pixel-menu part
-          isMenuVisible = true;
-          document.querySelector(".main-name-part").style.filter = "blur(3px)";
-          document.querySelector("#background-part-sec").style.filter = "blur(3px)";
-        } else {
-          pixelMenu.style.display = "none"; // Hide the pixel-menu part
-          isMenuVisible = false;
-          document.querySelector(".main-name-part").style.filter = "blur(0px)";
-          document.querySelector("#background-part-sec").style.filter = "blur(0px)";
-        }
-      });
-    }
-    // Call the function initially
-    toggleMenu();
+   function toggleMenu() {
+    const hamPartSection = document.querySelector(".ham-part-section");
+    const pixelMenu = document.querySelector(".pixel-menu");
+    let isMenuVisible = false;
+
+    hamPartSection.addEventListener("click", () => {
+      if (!isMenuVisible) {
+        pixelMenu.style.display = "flex"; // Show the pixel-menu part
+        isMenuVisible = true;
+        document.querySelector(".main-name-part").style.filter = "blur(3px)";
+        document.querySelector("#background-part-sec").style.filter = "blur(3px)";
+      } else {
+        pixelMenu.style.display = "none"; // Hide the pixel-menu part
+        isMenuVisible = false;
+        document.querySelector(".main-name-part").style.filter = "blur(0px)";
+        document.querySelector("#background-part-sec").style.filter = "blur(0px)";
+      }
+    });
   }
+  // Call the function initially
+  toggleMenu();
   
   // document.getElementById('animationButton').addEventListener('click', function() {
   //   setTimeout(function() {
