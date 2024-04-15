27f2a7b9ba2bbfe9567bfcf292d8d278207f8ba3
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-15 23:21:56 +0530

    TOURNAMENT PART 3

---
+++ js/tournament.js
@@ -0,0 +1,30 @@
+
+// TIME CLOCK FOR TOURNAMENT 
+
+// Set the date for the end of registration
+var endDate = new Date("April 20, 2024 00:00:00").getTime();
+
+// Update the countdown every second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+    
+  // Calculate the time remaining
+  var distance = endDate - now;
+    
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Display the countdown
+  document.getElementById("countdown").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+    
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Registration has ended";
+  }
+}, 1000);
