9e01680dcb91aa71db3ed943585e603ea2440450
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-23 14:04:54 +0530

    Tournament 13

--- js/tournament.js
+++ js/tournament.js
@@ -1,6 +1,3 @@
-
-// TIME CLOCK FOR TOURNAMENT 
-
 // Set the date for the end of registration
 var endDate = new Date("May 15, 2024 00:00:00").getTime();
 
@@ -12,19 +9,23 @@ var x = setInterval(function() {
     
   // Calculate the time remaining
   var distance = endDate - now;
-    
+  
   // Calculate days, hours, minutes, and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Display the countdown
-  document.getElementById("countdown").innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
-    
+  
+  // Display the countdown with each part wrapped in a div and spans below them
+  var countdownDisplay = '<div class="countdown-part">' + days + "<span>days</span></div> ";
+  countdownDisplay += '<div class="countdown-part">' + hours + "<span>hours</span></div> ";
+  countdownDisplay += '<div class="countdown-part">' + minutes + "<span>minutes</span></div> ";
+  countdownDisplay += '<div class="countdown-part">' + seconds + "<span>seconds</span></div> ";
+  document.getElementById("countdown").innerHTML = countdownDisplay;
+  
   // If the countdown is over, display a message
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "Registration has ended";
+    document.getElementById("countdown").innerHTML = "tournament has ended";
   }
 }, 1000);
