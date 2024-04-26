3511fe6bd8d8fc3afbdb2a0701c33c8ab4809006
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-26 09:31:22 +0530

    About section 2

---
+++ js/about.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const image = document.querySelector('.moving-image');
+
+container.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  
+  const containerRect = container.getBoundingClientRect();
+  const containerX = containerRect.left;
+  const containerY = containerRect.top;
+  
+  const imageHalfWidth = image.offsetWidth / 2;
+  const imageHalfHeight = image.offsetHeight / 2;
+  
+  const x = mouseX - containerX - imageHalfWidth;
+  const y = mouseY - containerY - imageHalfHeight;
+  
+  image.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+container.addEventListener('mouseleave', () => {
+  image.style.transform = 'translate(-50%, -50%)';
+});
