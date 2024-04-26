58148447f51b8f1ca87e0332aa22e66ac186f56d
Manthan Thakor <thakormanthan849@gmail.com>
2024-04-26 13:18:20 +0530

    About section 3

--- js/about.js
+++ js/about.js
@@ -1,23 +1,23 @@
-const container = document.querySelector('.container');
-const image = document.querySelector('.moving-image');
+// const container = document.querySelector('.container');
+// const image = document.querySelector('.moving-image');
 
-container.addEventListener('mousemove', (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+// container.addEventListener('mousemove', (e) => {
+//   const mouseX = e.clientX;
+//   const mouseY = e.clientY;
   
-  const containerRect = container.getBoundingClientRect();
-  const containerX = containerRect.left;
-  const containerY = containerRect.top;
+//   const containerRect = container.getBoundingClientRect();
+//   const containerX = containerRect.left;
+//   const containerY = containerRect.top;
   
-  const imageHalfWidth = image.offsetWidth / 2;
-  const imageHalfHeight = image.offsetHeight / 2;
+//   const imageHalfWidth = image.offsetWidth / 2;
+//   const imageHalfHeight = image.offsetHeight / 2;
   
-  const x = mouseX - containerX - imageHalfWidth;
-  const y = mouseY - containerY - imageHalfHeight;
+//   const x = mouseX - containerX - imageHalfWidth;
+//   const y = mouseY - containerY - imageHalfHeight;
   
-  image.style.transform = `translate(${x}px, ${y}px)`;
-});
+//   image.style.transform = `translate(${x}px, ${y}px)`;
+// });
 
-container.addEventListener('mouseleave', () => {
-  image.style.transform = 'translate(-50%, -50%)';
-});
+// container.addEventListener('mouseleave', () => {
+//   image.style.transform = 'translate(-50%, -50%)';
+// });
